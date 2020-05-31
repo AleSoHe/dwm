5f10d881b18e10835f60bd76d4d97d65e27e4546
Alejandro Solis <alejandrosohe@hotmail.com>
2020-05-31 10:45:58 -0600

    Fix a compilation error of the last commit
    I forgot the XK_* in the key definition, so there was a compilation error

--- config.h
+++ config.h
@@ -96,8 +96,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
     // Light
-    { MODKEY,                       z,         spawn,          SHCMD("light -U 5") },
-    { MODKEY,                       x,         spawn,          SHCMD("light -U 5") },
+    { MODKEY,                       XK_z,      spawn,          SHCMD("light -U 5") },
+    { MODKEY,                       XK_x,      spawn,          SHCMD("light -U 5") },
     // VOLUME
     { 0, XF86XK_AudioRaiseVolume,   spawn,     SHCMD("vol up; kill -44 $(pidof dwmblocks)") },
     { 0, XF86XK_AudioLowerVolume,   spawn,     SHCMD("vol down; kill -44 $(pidof dwmblocks)") },
