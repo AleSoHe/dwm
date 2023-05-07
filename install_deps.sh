b64e3e7170d853a0cdd93d9b08303006049a1af5
AleSoHe <alejandro.solis@ridgerun.com>
2023-05-07 14:19:42 -0600

    Add more instructions

--- install_deps.sh
+++ install_deps.sh
@@ -2,5 +2,14 @@
 
 sudo apt install make ttf-ancient-fonts-symbola build-essential libx11-dev libxft-dev libxinerama-dev libxcb1-dev libx11-xcb-dev libxcb-res0-dev
 
-
+echo "IMPORTANT: If using Ubuntu, follow the steps commented in this script below"
+# TODO: the following has to be done in Ubuntu too
+#sudo apt install dwm
+#sudo cp /usr/share/xsessions/dwm.desktop{,.bak}
+#sudo purge install dwm
+#sudo mv /usr/share/xsessions/dwm.desktop{.bak,}
+#
+#sudo apt install lightdm
+#
+#reboot, select dwm XSession when logging in
 
