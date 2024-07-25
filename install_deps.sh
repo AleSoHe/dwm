2061398206c9a3e138dc3737b584298772457d48
AleSoHe <alejandro.solis@ridgerun.com>
2024-07-24 18:05:15 -0600

    Add missing dependencies (light and dunst)

--- install_deps.sh
+++ install_deps.sh
@@ -3,6 +3,7 @@
 sudo apt install make ttf-ancient-fonts-symbola build-essential libx11-dev libxft-dev libxinerama-dev libxcb1-dev libx11-xcb-dev libxcb-res0-dev
 
 echo "IMPORTANT: If using Ubuntu, follow the steps commented in this script below"
+
 # TODO: the following has to be done in Ubuntu too
 #sudo apt install dwm
 #sudo cp /usr/share/xsessions/dwm.desktop{,.bak}
@@ -13,3 +14,9 @@ echo "IMPORTANT: If using Ubuntu, follow the steps commented in this script belo
 #
 #reboot, select dwm XSession when logging in
 
+# Backlight control
+sudo apt install light
+sudo chmod +s /usr/bin/light
+
+# Notifications controller
+sudo apt install dunst
